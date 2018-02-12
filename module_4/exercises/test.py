24abbf1918166d0cfd32619501662751702ae51b
rennitbaby <work.kevin.ren@gmail.com>
2018-02-11 23:17:29 -0800

    changes to test.py

--- module_4/exercises/test.py
+++ module_4/exercises/test.py
@@ -3,12 +3,20 @@
 import mechanize
 import urllib
 from bs4 import BeautifulSoup
+import threading
 
+threads = []
 
-br = mechanize.Browser()
-br.open("http://www.duckduckgo.com")
-html = br.response().read()
-bs = BeautifulSoup(html, 'lxml')
-print bs.prettify()
-print "="*100
-print html
+def anotherOne(browserInstance):
+#    print browserInstance.geturl()
+    pass
+def main():
+#    br = mechanize.Browser()
+ #   br.open("http://www.duckduckgo.com")
+  #  anotherOne(br)
+    for i in range(1,11):
+        t = threading.Thread(target=anotherOne, args=(i,))
+        threads.append(t)
+    print threads
+
+main()
