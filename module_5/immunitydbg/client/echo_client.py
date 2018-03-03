fe57872e80e4efd488a65f58544743f7cc2a265d
rennitbaby <work.kevin.ren@gmail.com>
2018-03-03 08:26:13 -0800

    fixed the client

--- module_5/immunitydbg/client/echo_client.py
+++ module_5/immunitydbg/client/echo_client.py
@@ -13,10 +13,14 @@ sock.connect(("192.168.1.81", 22000))
 
 buf = "A" * 20
 
-sock.send(buf)
 
-ech_re = sock.recv(1024)
-
-print ech_re
-
-#sock.close()
+try:
+    while (1):                              # infinite loop to keep sending bigger size buffers
+        sock.send(buf)
+        ech_re = sock.recv(1024)
+        print ech_re + "\n"
+        buf = buf * 2                       # incrementing the buff by x2 every loop        
+except KeyboardInterrupt:
+    print "\n[!] User requested shutdown..."
+    sock.close()
+    sys.exit(0)
