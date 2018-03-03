4250a07466d05ba1f992a216fe1c22497a7a2948
rennitbaby <work.kevin.ren@gmail.com>
2018-03-03 08:38:15 -0800

    commented increment part of client

--- module_5/immunitydbg/client/echo_client.py
+++ module_5/immunitydbg/client/echo_client.py
@@ -12,15 +12,17 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("192.168.1.81", 22000))
 
 buf = "A" * 20
+sock.send(buf)
+print sock.recv(1024)
 
-
-try:
-    while (1):                              # infinite loop to keep sending bigger size buffers
-        sock.send(buf)
-        ech_re = sock.recv(1024)
-        print ech_re + "\n"
-        buf = buf * 2                       # incrementing the buff by x2 every loop        
-except KeyboardInterrupt:
-    print "\n[!] User requested shutdown..."
-    sock.close()
-    sys.exit(0)
+# for incremental buffers, uncomment
+#try:
+#    while (1):                              # infinite loop to keep sending bigger size buffers
+#        sock.send(buf)
+#        ech_re = sock.recv(1024)
+#        print ech_re + "\n"
+#        buf = buf * 2                       # incrementing the buff by x2 every loop        
+#except KeyboardInterrupt:
+#    print "\n[!] User requested shutdown..."
+#    sock.close()
+#    sys.exit(0)
