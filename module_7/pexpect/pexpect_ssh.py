b91ff10117cfd5a12af2720adf3cde40d927c20d
rennitbaby <work.kevin.ren@gmail.com>
2018-05-17 22:30:07 -0700

    added pexpect ssh

--- module_7/pexpect/pexpect_ssh.py
+++ module_7/pexpect/pexpect_ssh.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-#NOTE: executing this script in ubuntu server (where our ftp server lives)
+
+#NOTE: executing this script in fedora ssh into 192.168.1.114 ubuntuserver16
 # importing the module we are going to use
 import pexpect
 import sys
@@ -8,25 +9,23 @@ import sys
 def main(argv):
     # first we have to use pexpect's spawn method 
     # using it as if we were typing the command on terminal
-    pex = pexpect.spawn('ftp localhost 45000')
+    pex = pexpect.spawn('ssh kevin@192.168.1.114')
     
     # and then we are expecting certain outputs as if it were to the terminal screen
     # expect method can also be used which can take a Regex
-    pex.expect_exact('Name')  # it searches for beginning of string
+    pex.expect_exact('password')  # it searches for beginning of string
     
     # once we have expected that Name string, we will have control of that specific line
-    pex.sendline(argv[0])
+    pex.sendline(argv)
     
+    user_host_home_dir = 'kevin@ubuntuServer:~$'
     # repeat for the following steps
-    pex.expect_exact('Password')
-    pex.sendline(argv[1])
-
-    pex.expect_exact('ftp')
+    pex.expect_exact(user_host_home_dir)
 
     pex.sendline('ls')
 
     # now we are expecting the next ftp line, so we can use the before method to see what was between giving the ls cmd and ftp
-    pex.expect_exact('ftp')
+    pex.expect_exact(user_host_home_dir)
 
     lines = pex.before.split('\n')
 
@@ -36,12 +35,12 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print 'please enter vsftp server username and password as params'
-        print 'format is: python pexpect_notes.py [ftp-server-name] [pw]'
-        print 'or: ./pexpect_notes.py [ftp-server-name] [pw]'
-        print 'example: ./pexpect_notes.py kevin-ftp password'
+    if len(sys.argv) < 2:
+        print 'please enter ssh password as cmd line param'
+        print 'format is: python pexpect_ssh.py [pw-IP]'
+        print 'or: ./pexpect_notes.py [pw-IP]'
+        print 'example: ./pexpect_ssh.py password'
         sys.exit(1)
     else:
-        main((sys.argv[1], sys.argv[2]))
+        main(sys.argv[1])
         sys.exit(0)
